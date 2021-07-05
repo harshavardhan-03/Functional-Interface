ea3e3c43ffd59653e37b6a93b4cabee70aa11dea
harshavardhan-03 <83867245+harshavardhan-03@users.noreply.github.com>
2021-07-05 23:19:07 +0530

    Delete CoverTester.java

--- CoverTester.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.functionalinterface;
-
-public class CoverTester {
-	public static void main(String[] args) {
-		Bag tester = new Bag();
-		tester.store();
-//		tester.reuse(true);
-	}
-}
